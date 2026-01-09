12f8f292e820a103c900dd1b0b634368ffa42d83
curdu <curdu01@gmail.com>
2026-01-09 21:12:32 +0100

    La classe Usuari ara implementa la interficie Serializable per facilitar futurs intercanvis d'informació

--- app/src/main/java/com/curdu/cafeflow/c_models/entitats/Usuari.kt
+++ app/src/main/java/com/curdu/cafeflow/c_models/entitats/Usuari.kt
@@ -1,7 +1,9 @@
 package com.curdu.cafeflow.c_models.entitats
 
+import java.io.Serializable
+
 data class Usuari (
     var nom : String,
     var contrasenya: String,
     var email: String
-)
+) : Serializable
