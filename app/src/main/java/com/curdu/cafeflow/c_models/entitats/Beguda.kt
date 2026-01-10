c73c938bb5c2b0ccbf087e9094ad946425ccf4f0
curdu <curdu01@gmail.com>
2026-01-10 01:03:34 +0100

    Reorganització del projecte en paquets a, b, c i implementació de l'entitat Beguda amb Room

---
+++ app/src/main/java/com/curdu/cafeflow/c_models/entitats/Beguda.kt
@@ -0,0 +1,21 @@
+package com.curdu.cafeflow.c_models.entitats
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "beguda")
+data class Beguda(
+    @ColumnInfo(name = "capacitat")
+    var capacitat: Int,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    override var id: Long,
+
+    @ColumnInfo(name = "nom")
+    override var nom: String,
+
+    @ColumnInfo(name = "preu")
+    override var preu: Double
+) : Producte(id, preu, nom)
--- app/src/main/java/com/curdu/cafeflow/viewmodels/BegudesVM.kt
+++
@@ -1,4 +0,0 @@
-package com.curdu.cafeflow.viewmodels
-
-class BegudesVM {
-}
